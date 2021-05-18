e4a0564cf09db12ec0b82324aa7b5f6fd22f8344
karlGusta <karlgustaesimit@gmail.com>
2021-05-18 06:02:02 +0300

    Added section new phones

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
@@ -12,13 +12,13 @@ $(document).ready(function(){
         nav: true,
         dots: false,
         responsive: {
-            0:{
+            0: {
                 items: 1
             },
-            600:{
+            600: {
                 items: 3
             },
-            1000:{
+            1000: {
                 items: 5
             }
         }
@@ -26,14 +26,34 @@ $(document).ready(function(){
 
     // isotope filter
     var $grid = $(".grid").isotope({
-        itemSelector:'.grid-item',
-        layoutMode:'fitRows'
-    });  
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
 
     // filter items on button click
-    $(".button-group").on("click","button", function(){
+    $(".button-group").on("click", "button", function () {
         var filterValue = $(this).attr('data-filter');
-        $grid.isotope({filter: filterValue});
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+
+    // New Phones  owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
     });
 });
-
