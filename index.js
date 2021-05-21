884608a36133dc255873fb8937f1fcda10d3a7e2
karlGusta <karlgustaesimit@gmail.com>
2021-05-21 06:00:06 +0300

    Added the 3 blog images

--- index.js
+++ index.js
@@ -57,19 +57,19 @@ $(document).ready(function () {
         }
     });
 
-        // blogs  owl carousel
-        $("#blogs .owl-carousel").owlCarousel({
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 5
-                }
+    // blogs  owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
             }
-        });
+        }
+    });
 
 });
