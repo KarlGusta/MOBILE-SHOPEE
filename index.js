0cb89ce8214d6226b79c72cd1fa5b5ea675e1dd3
karlGusta <karlgustaesimit@gmail.com>
2021-06-25 15:04:16 +0300

    Added data-id to qty items

--- index.js
+++ index.js
@@ -76,10 +76,11 @@ $(document).ready(function () {
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
 
-    let $input = $(".qty .qty-input");
+    //let $input = $(".qty .qty-input");
     
     // click on qty up button
     $qty_up.click(function(){
+        let $input = $(`.qty-input[data-id='${$(this).data("id")}']`);
         if($input.val()>=1 && $input.val()<=9){
             $input.val(function(i, oldval){
                 return ++oldval;
@@ -90,6 +91,7 @@ $(document).ready(function () {
 
     // Click on qty down button
     $qty_down.click(function(){
+        let $input = $(`.qty-input[data-id='${$(this).data("id")}']`);
         if($input.val()>1 && $input.val()<=10){
             $input.val(function(i, oldval){
                 return --oldval;
