29cfc96eae0a8e5b66d2e768d1895d0da74ed80c
karlGusta <karlgustaesimit@gmail.com>
2021-07-13 11:40:27 +0300

    Fetch product data one by one

--- Database/Product.php
+++ Database/Product.php
@@ -9,4 +9,17 @@ class Product {
         if(!isset($db->con)) return null;
         $this->db = $db;
     }
+
+    // Fetch product data using getData method
+
+    public function getData($table = 'product'){
+        $result = $this->db->con->query(query: "SELECT * FROM {$table}");
+
+        $resultArray = array();
+
+        // Fetch product data one by one 
+        while( $item = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $resultArray[] = $item;
+        }
+    }
 }
