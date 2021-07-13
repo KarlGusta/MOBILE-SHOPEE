92206315399c79cc50c0cb92c74cb56f2e5f078e
karlGusta <karlgustaesimit@gmail.com>
2021-07-13 11:13:34 +0300

    initialized db connection to the variable db

--- Database/Product.php
+++ Database/Product.php
@@ -2,5 +2,11 @@
 
 // Use to fetch product data
 class Product {
+    public $db = null;
 
+    public function __construct(DBController $db)
+    {
+        if(!isset($db->con)) return null;
+        $this->db = $db;
+    }
 }
