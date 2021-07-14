c716386ff156e11a65e1a0c18f29b907e805499b
karlGusta <karlgustaesimit@gmail.com>
2021-07-14 12:03:18 +0300

    The item names and prices  can also be gotten from the array

--- Template/_top-sale.php
+++ Template/_top-sale.php
@@ -12,9 +12,9 @@
             <?php foreach ($product_shuffle as $item) {?>
             <div class="item py-2">
                 <div class="product font-rale">
-                    <a href="#"><img src="<?php echo $item['item_image'] ?? './assets/products/1.png' ?>" alt="product1" class="img-fluid"></a>
+                    <a href="#"><img src="<?php echo $item['item_image'] ?? './assets/products/1.png'; ?>" alt="product1" class="img-fluid"></a>
                     <div class="text-center">
-                        <h6>Samsung Galaxy 10</h6>
+                        <h6><?php echo $item['item_name']?? 'Unknown'; ?></h6>
                         <div class="rating text-warning font-size-12">
                             <span><i class="fas fa-star"></i></span>
                             <span><i class="fas fa-star"></i></span>
@@ -23,7 +23,7 @@
                             <span><i class="far fa-star"></i></span>
                         </div>
                         <div class="price py-2">
-                            <span>$152</span>
+                            <span><?php echo $item['item_price'] ?? '0'; ?></span>
                         </div>
                         <button type="submit" class="btn btn-warning">Add to Cart</button>
                     </div>
